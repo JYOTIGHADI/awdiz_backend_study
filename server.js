266e339f47f03aea5948dd008bc56779ed687271
basic-html.css <basic-html.css.in>
2025-10-15 00:31:23 +0530

    day -3

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
 
 app.get("/", (req, res) => {
-  res.send("Welcome to the Express server!");
+  console.log(process.env.NAME, "- process.env.NAME");
+  console.log(process.env.MONGODB_URL, "- process.env.MONGODB_URL");
+
+  
+  process.env.NAME
+    ? res.send(` Hello ${process.env.NAME}! Welcome to the Express server!`)
+    : res.send(" Please check your .env file.");
 });
 
-app.post("/name", (req,res) =>{
-  res.send("hey i'm Jyoti")
-});
+
 
 app.listen(8000, () => {
   console.log("Server is running on http://localhost:8000");
